a17de418ea49677b1df48818f97648e17f2a19e6
Joel Sticha (jsticha) <joel.sticha@optum.com>
2022-09-06 16:25:42 -0500

    Transferred all old repos from `riptide-team`

--- src/main/java/com/optum/riptide/devops/githubmetricsapi/GithubMetricsApiApplication.java
+++ src/main/java/com/optum/riptide/devops/githubmetricsapi/GithubMetricsApiApplication.java
@@ -3,7 +3,11 @@ package com.optum.riptide.devops.githubmetricsapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {
+      "com.optum.riptide.http.restclients",
+      "com.optum.riptide.devops.githubmetricsapi"
+    })
 public class GithubMetricsApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(GithubMetricsApiApplication.class, args);
