997be320fc55bf516eb39a6fcb82bb79e9de0a56
Sticha, Joel (jsticha) <joel.sticha@optum.com>
2022-11-29 09:05:35 -0600

    Delete unused method

--- src/test/groovy/com/optum/riptide/devops/githubmetricsapi/vault/PasswordVaultingReport_Job.groovy
+++ src/test/groovy/com/optum/riptide/devops/githubmetricsapi/vault/PasswordVaultingReport_Job.groovy
@@ -108,6 +108,4 @@ class PasswordVaultingReport_Job extends Specification {
 //    'ACET-Automation'         | 'mysql and vault report' | 'ACET-Automation mysql and vault report.xlsx'
   }
 
-  def hasVault() {}
-
 }
