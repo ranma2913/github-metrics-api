c7cd1e53b7852c5ec883e804599d55999ea6ea74
asushma <aytla_sushma@optum.com>
2022-12-06 21:02:29 +0530

    intial commit for CodeQL report generation

---
+++ src/test/groovy/com/optum/riptide/devops/githubmetricsapi/codeQL/CodeQLReport_Job.groovy
@@ -0,0 +1,63 @@
+package com.optum.riptide.devops.githubmetricsapi.codeQL
+
+import com.optum.riptide.devops.githubmetricsapi.codeQL.CodeQLService
+import com.optum.riptide.devops.githubmetricsapi.utils.FileWriterUtilForCodeQL
+import groovy.util.logging.Slf4j
+import org.kohsuke.github.GHOrganization
+import org.kohsuke.github.GHRepository
+import org.kohsuke.github.GitHub
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.time.LocalDateTime
+
+@SpringBootTest
+@Slf4j
+class CodeQLReport_Job extends Specification {
+  @Autowired
+  GitHub githubEnterprise
+  @Autowired
+  CodeQLService codeQLService
+
+  @Unroll("Create csv of repos missing vitals.yaml for org = #orgName")
+  def "Create csv of repos missing vitals.yaml"() {
+    given:
+    def csvHeadRow = ['Repository', 'Repo URL', 'Default Branch', 'Code QL Merge status'] // Header Row
+    def csvData = []
+    GHOrganization org = githubEnterprise.getOrganization(orgName)
+    List<GHRepository> repositories = org.listRepositories(100).toList()
+
+    Path outputFilePath = Paths.get("target/${LocalDateTime.now().toString().replace(':', '')}_$outputFileName")
+    Files.createDirectories(outputFilePath.getParent())
+    outputFilePath = Files.createFile(outputFilePath)
+
+    List<GHRepository> filteredRepos =
+        repositories.parallelStream()
+            .map(
+                repo -> {
+                  def csvRow = [repo.getFullName(), repo.getHtmlUrl(), repo.getDefaultBranch(), codeQLService.isCodeQLFileMerged(repo)]
+                  csvData.add(csvRow)
+                })
+            .toList()
+    if (outputFilePath.toString().contains("xls")) {
+        FileWriterUtilForCodeQL.writeSimpleXlsxFile(outputFilePath, csvHeadRow, csvData, sheetName)
+    } else {
+        FileWriterUtilForCodeQL.writeCsvFile(outputFilePath, [csvHeadRow] + csvData)
+    }
+
+    expect:
+    filteredRepos.size() == repositories.size()
+
+    where:
+    orgName        | sheetName           | outputFileName
+//    'riptide-deprecated-apps' | 'Needs vitals.yaml' | 'riptide-deprecated-apps_missing_vitals.xlsx'
+//    'riptide-devops'          | 'Needs vitals.yaml' | 'riptide-devops_missing_vitals.xlsx'
+//    'riptide-poc'             | 'Needs vitals.yaml' | 'riptide-poc_missing_vitals.xlsx'
+    'riptide-team' | 'Needs vitals.yaml' | 'riptide-team_codeQL_report.xlsx'
+  }
+}
