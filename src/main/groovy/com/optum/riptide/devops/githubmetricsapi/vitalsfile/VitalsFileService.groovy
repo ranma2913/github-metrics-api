79bec2c6be70abc53961ae4c00de363d2939aee8
asushma <aytla_sushma@optum.com>
2022-12-09 00:14:26 +0530

    revert

--- src/main/groovy/com/optum/riptide/devops/githubmetricsapi/vitalsfile/VitalsFileService.groovy
+++ src/main/groovy/com/optum/riptide/devops/githubmetricsapi/vitalsfile/VitalsFileService.groovy
@@ -62,8 +62,8 @@ class VitalsFileService {
 
   @Autowired
   VitalsFileService(
-          @Value('${uhg.vitals-file.schema}') String vitalsFileSchemaUrl,
-          @Value('${classpath:vitals_yaml_schema.json}') ClassPathResource localSchemaJson) {
+      @Value('${uhg.vitals-file.schema}') String vitalsFileSchemaUrl,
+      @Value('${classpath:vitals_yaml_schema.json}') ClassPathResource localSchemaJson) {
 
     yamlSlurper = new YamlSlurper()
 
@@ -80,16 +80,16 @@ class VitalsFileService {
     List<GHRepository> repositories = github.getOrganization(org).listRepositories(100).toList()
 
     updatedRepositories =
-            repositories.stream()
-                    .map(
-                            repo -> {
-                              try {
-                                return createMissingVitalsFileInRepo(repo, false, true)
-                              } catch (IOException e) {
-                                throw new RuntimeException(e)
-                              }
-                            })
-                    .toList()
+        repositories.stream()
+            .map(
+                repo -> {
+                  try {
+                    return createMissingVitalsFileInRepo(repo, false, true)
+                  } catch (IOException e) {
+                    throw new RuntimeException(e)
+                  }
+                })
+            .toList()
 
     return Flux.fromIterable(updatedRepositories)
   }
@@ -104,72 +104,23 @@ class VitalsFileService {
     List<GHRepository> repositories = github.getOrganization(org).listRepositories(100).toList()
 
     updatedRepositories =
-            repositories.stream()
-                    .map(
-                            repo -> {
-                              try {
-                                return createMissingVitalsFileInRepo(repo, enablePoc, overrideBranchProtection)
-                              } catch (IOException e) {
-                                throw new RuntimeException(e)
-                              }
-                            })
-                    .toList()
+        repositories.stream()
+            .map(
+                repo -> {
+                  try {
+                    return createMissingVitalsFileInRepo(repo, enablePoc, overrideBranchProtection)
+                  } catch (IOException e) {
+                    throw new RuntimeException(e)
+                  }
+                })
+            .toList()
 
     return Flux.fromIterable(updatedRepositories)
-  }
-
-  Flux<GHRepository> createCodeQLFilesRepoInOrg(String org, boolean enablePoc, boolean overrideBranchProtection) throws IOException {
-
-    Map<GHRepository, Boolean> updatedRepositories
-    List<GHRepository> repositories = github.getOrganization(org).listRepositories(100).toList()
-
-    updatedRepositories =
-            repositories.stream()
-                    .map(
-                            repo -> {
-                              try {
-                                return createCodeQLFileInRepo(repo, enablePoc, overrideBranchProtection)
-                              } catch (IOException e) {
-                                throw new RuntimeException(e)
-                              }
-                            })
-                    .toList()
-
-    return Flux.fromIterable(updatedRepositories)
-  }
-
-  Map<GHRepository, Boolean> createCodeQLFileInRepo(
-          GHRepository repo, boolean enablePoc, boolean overrideBranchProtection) throws IOException {
-    GHRepository codeQLMergedRepo = null
-    Boolean isMerged;
-    Map<GHRepository, Boolean> repoDetails = new HashMap<GHRepository, Boolean>();
-    try {
-    Optional<GHContent> vitalsFileOptional = this.getExistingCodeQLFile(repo)
-      if (vitalsFileOptional.present) {
-        log.debug('vitals.yaml exists in repo {}', repo.htmlUrl)
-        codeQLMergedRepo = repo
-        isMerged = true
-
-      } else {
-        log.warn('vitals.yaml not found in repo {}', repo.htmlUrl)
-        codeQLMergedRepo = repo
-        isMerged = false
-      }
-      repoDetails.put(codeQLMergedRepo, isMerged)
-    } catch(FileNotFoundException e){
-      codeQLMergedRepo = repo
-      isMerged = false
-    }
-    catch(Exception e){
-      codeQLMergedRepo = repo
-      isMerged = false
-    }
-    return repoDetails;
   }
 
   /** create a vitals file if it's missing. Return null if not updated. */
   GHRepository createMissingVitalsFileInRepo(
-          GHRepository repo, boolean enablePoc, boolean overrideBranchProtection) throws IOException {
+      GHRepository repo, boolean enablePoc, boolean overrideBranchProtection) throws IOException {
     Optional<GHContent> vitalsFileOptional = this.getExistingVitalsFile(repo)
     GHRepository updatedRepo = null
     if (vitalsFileOptional.present) {
@@ -179,9 +130,9 @@ class VitalsFileService {
       log.warn('vitals.yaml not found in repo {}', repo.htmlUrl)
       if (repo.archived) {
         log.error(
-                'Repository {} is archived. Please go to the GitHub Web UI to unarchive: {}',
-                repo.fullName,
-                repo.htmlUrl + '/settings')
+            'Repository {} is archived. Please go to the GitHub Web UI to unarchive: {}',
+            repo.fullName,
+            repo.htmlUrl + '/settings')
       } else {
         GHBranchProtection origProtection = null
         if (overrideBranchProtection) {
@@ -193,14 +144,14 @@ class VitalsFileService {
         }
         VitalsFile vitalsFile = new VitalsFile()
         vitalsFile
-                .metadata.projectFriendlyName = pomParserService.readProjectFriendlyName(repo, POM_FILE)
+            .metadata.projectFriendlyName = pomParserService.readProjectFriendlyName(repo, POM_FILE)
         vitalsFile.metadata.projectKey = pomParserService.readProjectKey(repo, POM_FILE)
 
         // 1. check compliance.yaml
         var caAgileId = complianceFileService.readCaAgileId(repo)
         // 2. check Optumfile.yml
         caAgileId =
-                StringUtils.isNotBlank(caAgileId) ? caAgileId : optumFileService.readCaAgileId(repo)
+            StringUtils.isNotBlank(caAgileId) ? caAgileId : optumFileService.readCaAgileId(repo)
         // 3. use 'poc' if not found
         if (enablePoc) {
           caAgileId = StringUtils.isNotBlank(caAgileId) ? caAgileId : 'poc'
@@ -213,27 +164,27 @@ class VitalsFileService {
           ObjectMapper mapper = new ObjectMapper(new YAMLFactory())
           String vitalsFileString = mapper.writeValueAsString(vitalsFile)
           GHContentUpdateResponse response =
-                  repo.createContent()
-                          .path(VITALS_FILE)
-                          .content(vitalsFileString)
-                          .message('Create ' + VITALS_FILE)
-                          .commit()
+              repo.createContent()
+                  .path(VITALS_FILE)
+                  .content(vitalsFileString)
+                  .message('Create ' + VITALS_FILE)
+                  .commit()
 
           log.info(
-                  '{} file successfully added with commit {}',
-                  VITALS_FILE,
-                  response.commit.sha)
+              '{} file successfully added with commit {}',
+              VITALS_FILE,
+              response.commit.sha)
           updatedRepo = repo
         } else {
           log.error(
-                  'Unable to read caAgileId from compliance.yaml or Optumfile.yml in repo {}',
-                  repo.htmlUrl)
+              'Unable to read caAgileId from compliance.yaml or Optumfile.yml in repo {}',
+              repo.htmlUrl)
         }
 
         // Put back protection if any.
         if (null != origProtection) {
           branchProtectionService.protectBranch(
-                  repo.getBranch(repo.defaultBranch), origProtection)
+              repo.getBranch(repo.defaultBranch), origProtection)
         }
       }
     }
@@ -245,7 +196,7 @@ class VitalsFileService {
     Optional<GHContent> vitalsFileOptional = this.getExistingVitalsFile(repo)
     if (vitalsFileOptional.present) {
       GHContentUpdateResponse updateResponse = vitalsFileOptional.get()
-              .update(newVitalsFile.toString(), 'Updating vitals.yaml content.')
+          .update(newVitalsFile.toString(), 'Updating vitals.yaml content.')
       log.info('Updated vitals.yaml in commit = {}', updateResponse.commit.htmlUrl)
     }
     return vitalsFileOptional
@@ -253,21 +204,6 @@ class VitalsFileService {
 
   Optional<GHContent> getExistingVitalsFile(GHRepository repo) throws IOException {
     return contentHelper.getFileContent(repo, '/', VITALS_FILE)
-  }
-
-  Boolean isCodeQLFileMerged(GHRepository repo) throws IOException {
-      Optional<GHContent> complianceFileOptional = null;
-      Boolean isCodeQLMerged
-      try {
-          complianceFileOptional = contentHelper.getFileContent(repo, '/.github/workflows/', "codeql-analysis.yml")
-          if(complianceFileOptional.isPresent()){
-              isCodeQLMerged = true;
-          }
-      }catch(FileNotFoundException e){
-          log.error("code QL file not found exception")
-          isCodeQLMerged = false
-      }
-    return isCodeQLMerged
   }
 
   Set<ValidationMessage> validateVitalsFile(GHContent vitalsFileContent) throws IOException {
@@ -278,9 +214,9 @@ class VitalsFileService {
     } catch (Exception e) {
       log.error('Unable to validate {}', vitalsFileContent.getHtmlUrl(), e)
       def customValidationMessage = ValidationMessage.of('Parse error',
-              new CustomErrorMessageType('FATAL',
-                      new MessageFormat("Unable to parse ${vitalsFileContent.getHtmlUrl()}, cause = ${e.getLocalizedMessage()}")),
-              "${e.getClass()}", vitalsFileContent.getHtmlUrl(), e.getLocalizedMessage())
+          new CustomErrorMessageType('FATAL',
+              new MessageFormat("Unable to parse ${vitalsFileContent.getHtmlUrl()}, cause = ${e.getLocalizedMessage()}")),
+          "${e.getClass()}", vitalsFileContent.getHtmlUrl(), e.getLocalizedMessage())
       log.debug('Custom ValidationMessage = {}', customValidationMessage)
       validationMessages.add(customValidationMessage)
     }
@@ -311,7 +247,7 @@ class VitalsFileService {
               try {
                 def historicVitals = yamlSlurper.parse(repo.getFileContent('vitals.yaml', commitSha).read())
                 if (historicVitals.metadata.caAgileId != 'poc' &&
-                        historicVitals.metadata.caAgileId.replaceAll('[ud]', '').isNumber()) {
+                    historicVitals.metadata.caAgileId.replaceAll('[ud]', '').isNumber()) {
                   vitalsFile.metadata.caAgileId = historicVitals.metadata.caAgileId
                   break // exit from commit loop
                 }
@@ -360,6 +296,4 @@ class VitalsFileService {
     }
     return responseOptional
   }
-
-
 }
