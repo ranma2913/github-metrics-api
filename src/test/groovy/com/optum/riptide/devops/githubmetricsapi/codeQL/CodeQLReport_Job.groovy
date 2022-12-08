89b845c131315250ed81ce3bfb4d3ef3702b26a3
asushma <aytla_sushma@optum.com>
2022-12-08 23:47:25 +0530

    format code

--- src/test/groovy/com/optum/riptide/devops/githubmetricsapi/codeQL/CodeQLReport_Job.groovy
+++ src/test/groovy/com/optum/riptide/devops/githubmetricsapi/codeQL/CodeQLReport_Job.groovy
@@ -1,31 +1,22 @@
 package com.optum.riptide.devops.githubmetricsapi.codeQL
 
-import com.google.gson.Gson
-import com.optum.riptide.devops.githubmetricsapi.codeQL.CodeQLService
-import com.optum.riptide.devops.githubmetricsapi.codeQL.ScanRepoClasses.CodeQlAlerts
-import com.optum.riptide.devops.githubmetricsapi.codeQL.ScanRepoClasses.Rule
 import com.optum.riptide.devops.githubmetricsapi.utils.CellProps
 import com.optum.riptide.devops.githubmetricsapi.utils.FileWriterUtil
-import com.optum.riptide.devops.githubmetricsapi.utils.FileWriterUtilForCodeQL
 import groovy.util.logging.Slf4j
-import org.json.JSONObject
 import org.kohsuke.github.GHOrganization
 import org.kohsuke.github.GHRepository
 import org.kohsuke.github.GitHub
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
-import org.springframework.web.client.RequestCallback
 import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
 import spock.lang.Unroll
 
-import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.time.LocalDateTime
@@ -33,80 +24,75 @@ import java.time.LocalDateTime
 @SpringBootTest
 @Slf4j
 class CodeQLReport_Job extends Specification {
-  @Autowired
-  GitHub githubEnterprise
-  @Autowired
-  CodeQLService codeQLService
-  @Autowired
-  RestTemplate sslRestTemplate
-  @Value('${credentials_GIT_TOKEN}')
-  String githubToken
+    @Autowired
+    GitHub githubEnterprise
+    @Autowired
+    CodeQLService codeQLService
+    @Autowired
+    RestTemplate sslRestTemplate
+    @Value('${credentials_GIT_TOKEN}')
+    String githubToken
 
-  @Unroll("Create csv of repos Code QL repo for org = #orgName")
-  def "Create csv of repos Code QL status"() {
-    given:
-    List<CellProps> headerRow = [
-            new CellProps('Repository', 'String'),
-            new CellProps('Repo URL', 'String'),
-            new CellProps('Default Branch', 'String'),
-            new CellProps('Code QL Merge status', 'Boolean'),
-            new CellProps('critical', 'String'),
-            new CellProps('high', 'String'),
-            new CellProps('medium', 'String'),
-            new CellProps('low', 'String'),
-            new CellProps('severity Error Count', 'String'),
-            new CellProps('severity Note Count', 'String'),
-            new CellProps('severity Warning Count', 'String'),
-            new CellProps('Additional Notes', 'String'),
-    ]
-    GHOrganization org = githubEnterprise.getOrganization(orgName)
-    List<GHRepository> repositories = org.listRepositories(100).toList()
+    @Unroll("Create csv of repos Code QL repo for org = #orgName")
+    def "Create csv of repos Code QL status"() {
+        given:
+        List<CellProps> headerRow = [new CellProps('Repository', 'String'),
+                                     new CellProps('Repo URL', 'String'),
+                                     new CellProps('Default Branch', 'String'),
+                                     new CellProps('Code QL Merge status', 'Boolean'),
+                                     new CellProps('critical', 'String'),
+                                     new CellProps('high', 'String'),
+                                     new CellProps('medium', 'String'),
+                                     new CellProps('low', 'String'),
+                                     new CellProps('severity Error Count', 'String'),
+                                     new CellProps('severity Note Count', 'String'),
+                                     new CellProps('severity Warning Count', 'String'),
+                                     new CellProps('Additional Notes', 'String'),]
+        GHOrganization org = githubEnterprise.getOrganization(orgName)
+        List<GHRepository> repositories = org.listRepositories(100).toList()
 
-    List<List<CellProps>> dataRows = new LinkedList()
+        List<List<CellProps>> dataRows = new LinkedList()
 
-     when: 'read the data'
-     dataRows.addAll(readCsvDataForOrg(orgName,repositories))
+        when: 'read the data'
+        dataRows.addAll(readCsvDataForOrg(orgName, repositories))
 
-     then: 'export the file'
-     Path outputFilePath = Paths.get("target/${LocalDateTime.now().toString().replace(':', '')}_$outputFileName")
-     FileWriterUtil.writeXlsxFile(outputFilePath, headerRow, dataRows, sheetName)
+        then: 'export the file'
+        Path outputFilePath = Paths.get("target/${LocalDateTime.now().toString().replace(':', '')}_$outputFileName")
+        FileWriterUtil.writeXlsxFile(outputFilePath, headerRow, dataRows, sheetName)
 
-    expect:
-    repositories.size() == dataRows.size()
+        expect:
+        repositories.size() == dataRows.size()
 
-    where:
-    orgName        | sheetName           | outputFileName
+        where:
+        orgName        | sheetName            | outputFileName
 //    'riptide-deprecated-apps' | 'Needs vitals.yaml' | 'riptide-deprecated-apps_missing_vitals.xlsx'
 //    'riptide-devops'          | 'Needs vitals.yaml' | 'riptide-devops_missing_vitals.xlsx'
 //    'riptide-poc'             | 'Needs vitals.yaml' | 'riptide-poc_missing_vitals.xlsx'
-    'riptide-team' | 'Needs code QL file' | 'riptide-team_codeQL_report.xlsx'
-  }
+        'riptide-team' | 'Needs code QL file' | 'riptide-team_codeQL_report.xlsx'
+    }
 
     List<List<CellProps>> readCsvDataForOrg(String orgName, List<GHRepository> repositories) {
         def csvData = []
         List<GHRepository> filteredRepos =
                 repositories.parallelStream()
-                        .map(
-                                repo -> {
-                                    Map<String, Integer> codeSeverityCounts = getCodeQLIssueCount(repo, orgName)
-                                    List<CellProps> dataRow = [
-                                            new CellProps("${(repo.getFullName()?.trim() ?: "${repo.getOwner()}/${repo.getName()}")}", 'String'),
-                                            new CellProps(repo.getHtmlUrl() as String, 'URL'),
-                                            new CellProps(repo.getDefaultBranch() as String, 'String'),
-                                            new CellProps(codeQLService.isCodeQLFileMerged(repo) as String, 'String'),
-                                            new CellProps(codeSeverityCounts.get("critical") as String, 'String'),
-                                            new CellProps(codeSeverityCounts.get("high") as String, 'String'),
-                                            new CellProps(codeSeverityCounts.get("medium") as String, 'String'),
-                                            new CellProps(codeSeverityCounts.get("low") as String, 'String'),
-                                            new CellProps(codeSeverityCounts.get("severity Error Count") as String, 'String'),
-                                            new CellProps(codeSeverityCounts.get("severity Note Count") as String, 'String'),
-                                            new CellProps(codeSeverityCounts.get("severity Warning Count") as String, 'String'),
-                                            new CellProps(codeSeverityCounts.get("Additional Notes") as String, 'String')
-                                    ]
+                        .map(repo -> {
+                            Map<String, Integer> codeSeverityCounts = getCodeQLIssueCount(repo, orgName)
+                            List<CellProps> dataRow = [new CellProps("${(repo.getFullName()?.trim() ?: "${repo.getOwner()}/${repo.getName()}")}", 'String'),
+                                                       new CellProps(repo.getHtmlUrl() as String, 'URL'),
+                                                       new CellProps(repo.getDefaultBranch() as String, 'String'),
+                                                       new CellProps(codeQLService.isCodeQLFileMerged(repo) as String, 'String'),
+                                                       new CellProps(codeSeverityCounts.get("critical") as String, 'String'),
+                                                       new CellProps(codeSeverityCounts.get("high") as String, 'String'),
+                                                       new CellProps(codeSeverityCounts.get("medium") as String, 'String'),
+                                                       new CellProps(codeSeverityCounts.get("low") as String, 'String'),
+                                                       new CellProps(codeSeverityCounts.get("severity Error Count") as String, 'String'),
+                                                       new CellProps(codeSeverityCounts.get("severity Note Count") as String, 'String'),
+                                                       new CellProps(codeSeverityCounts.get("severity Warning Count") as String, 'String'),
+                                                       new CellProps(codeSeverityCounts.get("Additional Notes") as String, 'String')]
 
-                                   // def csvRow = [repo.getFullName(), repo.getHtmlUrl(), repo.getDefaultBranch(), codeQLService.isCodeQLFileMerged(repo)]
-                                    csvData.add(dataRow)
-                                })
+                            // def csvRow = [repo.getFullName(), repo.getHtmlUrl(), repo.getDefaultBranch(), codeQLService.isCodeQLFileMerged(repo)]
+                            csvData.add(dataRow)
+                        })
                         .toList()
         return csvData
     }
@@ -121,28 +107,26 @@ class CodeQLReport_Job extends Specification {
         Map<String, Integer> codeSeverityCountMap = new HashMap<>();
 
         try {
-            responseEntity = sslRestTemplate.exchange(
-                    "${githubEnterprise.getApiUrl()}/repos/{owner}/{repo}/code-scanning/alerts",
+            responseEntity = sslRestTemplate.exchange("${githubEnterprise.getApiUrl()}/repos/{owner}/{repo}/code-scanning/alerts",
                     HttpMethod.GET,
                     requestEntity,
                     ArrayList.class,
-                    [owner: repo.getOwnerName(), repo: repo.getName(), org: orgName]
-            )
+                    [owner: repo.getOwnerName(), repo: repo.getName(), org: orgName])
             List<Objects> resp = responseEntity.getBody()
 
-            if(resp.isEmpty()){
+            if (resp.isEmpty()) {
                 codeSeverityCountMap.put("Additional Notes", "This branch hasn't been scanned yet")
                 log.info("This branch hasn't been scanned yet {} {}", repo.getName(), codeSeverityCountMap.toString())
                 return codeSeverityCountMap;
-            } else{
+            } else {
                 getScanCounts(resp, codeSeverityCountMap)
-                log.info("responseEntity.getStatusCode = {} {}", codeSeverityCountMap.toString(),repo.getName())
+                log.info("responseEntity.getStatusCode = {} {}", codeSeverityCountMap.toString(), repo.getName())
             }
 
         } catch (Exception e) {
             log.error("Exception calling code-scanning/alerts API: ", repo.getName())
             codeSeverityCountMap.put("Additional Notes", e.getMessage())
-            log.info("responseEntity.getStatusCode = {} {}", codeSeverityCountMap.toString(),repo.getName())
+            log.info("responseEntity.getStatusCode = {} {}", codeSeverityCountMap.toString(), repo.getName())
         }
         return codeSeverityCountMap;
     }
